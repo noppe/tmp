bd5e9905594bbbb77b32c8d2bbf70c2593f47fa5
Nils Olof Paulsson <n.o.paulsson@gmail.com>
2014-07-29 22:07:05 +0200

    Add a pie

--- com.vogella.git.multi.java1/src/main.java
+++ com.vogella.git.multi.java1/src/main.java
@@ -8,6 +8,7 @@ public class main {
 		// TODO Auto-generated method stub
 		System.out.println ("Hello world1") ;
 		System.out.println ("Hello world2") ;
+		System.out.println ("Hello world 3.141592653589793") ;
 	}
 
 }
